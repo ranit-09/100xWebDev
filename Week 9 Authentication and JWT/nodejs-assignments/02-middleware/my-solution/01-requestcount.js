3a2f5687964cc0d0c61f5c8eab0b7d823d3fdf0b
Ranit Banik <ranitbnk06@gmail.com>
2026-03-19 22:47:29 +0530

    nodejs assignments middleware completed

---
+++ Week 9 Authentication and JWT/nodejs-assignments/02-middleware/my-solution/01-requestcount.js
@@ -0,0 +1,32 @@
+// You have to create a middleware for logging the number of requests on a server
+
+const express = require('express');
+
+const app = express();
+let requestCount = 0;
+
+// You have been given an express server which has a few endpoints.
+// Your task is to create a global middleware (app.use) which will
+// maintain a count of the number of requests made to the server in the global
+// requestCount variable
+
+// Global middleware to log requests
+app.use((req, res, next) => {
+  requestCount += 1; // Increment the request count for every incoming request
+  next(); // Pass control to the next middleware or route handler
+});
+
+app.get('/user', function(req, res) {
+  res.status(200).json({ name: 'john' });
+});
+
+app.post('/user', function(req, res) {
+  res.status(200).json({ msg: 'created dummy user' });
+});
+
+app.get('/requestCount', function(req, res) {
+  res.status(200).json({ requestCount });
+});
+
+module.exports = app;
+
---
+++ Week 9 Authentication and JWT/nodejs-assignments/02-middleware/my-solution/02-authmiddleware.js
@@ -0,0 +1,29 @@
+
+//  Implement an authentication middleware that checks for a valid API key in the request headers.
+
+const express = require('express');
+const app = express();
+const VALID_API_KEY = '100xdevs_cohort3_super_secret_valid_api_key'; // key is 100xdevs-api-key use that API key for authenticate user
+
+
+// Middleware to check for a valid API key
+function authenticateAPIKey(req, res, next) {
+    //  authenticate APIKey here
+    const api_key = req.headers['100xdevs-api-key']
+    if (api_key === VALID_API_KEY) {
+        return next();
+    } else {
+        return res.status(401).send({ message: "Invalid or missing API key" })
+    }
+}
+
+app.use(authenticateAPIKey);
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Access granted' });
+});
+
+module.exports = app;
+
+
+
